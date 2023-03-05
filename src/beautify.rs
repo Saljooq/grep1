4872bc9063f0c492e4e8f58a677abe6044948abd
Saljooq <altaf.saljooq@outlook.com>
2023-03-05 12:53:39 -0600

    Refactored the logic for beautifying the output too

---
+++ src/beautify.rs
@@ -0,0 +1,103 @@
+use std::str::Lines;
+
+use colored::Colorize;
+use term_size;
+
+pub enum State {
+    Header,
+    Body,
+}
+
+pub struct BeautifyLines {
+    state: State,
+    string: String,
+}
+
+
+impl BeautifyLines {
+
+    pub fn new(state: State, string: String) -> BeautifyLines {
+        BeautifyLines {
+            state: state,
+            string: string,
+        }
+
+    }
+
+
+    pub fn print (&self) {
+
+        match self.state {
+            State::Header => self.print_header(),
+            State::Body => self.print_body(),
+        }
+
+    }
+
+
+    fn print_header(&self) {
+
+        let vec_str = Self::limit_lines(self.string.lines());
+
+        let header_str = vec_str.get(0).expect("We need at least one line of output to populate header");
+
+        print!("{}\n", header_str.on_black().bright_white().bold());
+    
+        for _ in 0..Self::get_max_len() {
+            print!("=")
+        }
+
+        println!();
+
+    }
+
+
+    fn print_body(&self) {
+
+        let vec_str = Self::limit_lines(self.string.lines());
+
+        for i in 0..vec_str.len() {
+
+            let line = vec_str.get(i).expect("");
+    
+            let colored_line = match i  % 4 {
+                0 => line.red(),
+                1 => line.bright_green(),
+                2 => line.yellow(),
+                _ => line.bright_magenta(),
+            };
+    
+    
+            println! (
+                "{}", 
+                colored_line
+            );
+        }
+    
+        
+    }
+
+
+    fn limit_lines(lines: Lines) -> Vec<&str> {
+
+        let max_len = Self::get_max_len();
+
+        let output_new = lines.map(|line| {
+            if line.len() < max_len {
+                return line
+            }
+    
+            &line[0..max_len]
+        }).collect::<Vec<&str>>();
+
+        output_new
+
+    }
+
+    fn get_max_len() -> usize {
+        let max_len = term_size::dimensions().expect("Couldn't get the terminal size").0;
+        max_len
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,35 @@
 
 use std::env;
-use term_size;
-use colored::Colorize;
-
 mod child;
+mod beautify;
+
 use child::ChildPipedProcess;
+use beautify::BeautifyLines;
 
-
-// SOLUTION: https://stackoverflow.com/questions/49218599/write-to-child-process-stdin-in-rust
 fn main() {
 
     
     let header_str = ChildPipedProcess::new("head", &["-n1"])
         .process_output()
         .output_to_str();
+
+    BeautifyLines::new(beautify::State::Header,header_str).print();
     
     let args = env::args()
         .collect::<Vec<_>>();
     
-    let grep_arg = args
-        .get(1)
-        .expect("You need atleast one argument for grep");
-    
-    let grep_str = ChildPipedProcess::new("grep", &[grep_arg]).process_output().output_to_str();
+    match args.get(1) {
+        Some(arg) => {
+            let grep_str = ChildPipedProcess::new("grep", &[arg]).process_output().output_to_str();
+            BeautifyLines::new(beautify::State::Body, grep_str).print();
+        },
+        None => {
+            println!("To see the grep match add an argument, for now printing all...");
 
-
-    let lines = grep_str.lines();
-
-    let max_len = term_size::dimensions().expect("Couldn't get the terminal size").0;
-
-    let output_new = lines.map(|line| {
-        if line.len() < max_len {
-            return line
+            let grep_str = ChildPipedProcess::new("cat", &[]).process_output().output_to_str();
+            BeautifyLines::new(beautify::State::Body, grep_str).print();
         }
-
-        &line[0..max_len]
-    }).collect::<Vec<&str>>();
-
-    print!("{}", header_str.on_black().bright_white().bold());
-    
-    for _ in 0..max_len {
-        print!("=")
     }
-
-    println!();
-
-    for i in 0..output_new.len() {
-
-        let line = output_new.get(i).expect("");
-
-        let colored_line = match i  % 4 {
-            0 => line.red(),
-            1 => line.bright_green(),
-            2 => line.yellow(),
-            _ => line.bright_magenta(),
-        };
-
-
-        println! (
-            "{}", 
-            colored_line
-        );
-    }
-
+        
 
 }
