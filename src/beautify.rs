47bccec1b435edbf9f11da32eb854f5bdfeb0fa6
Saljooq <altaf.saljooq@outlook.com>
2023-03-12 19:15:44 -0500

    Updated formatting - also renamed to grep1 for clarity

--- src/beautify.rs
+++ src/beautify.rs
@@ -1,5 +1,5 @@
-use std::str::{Lines};
-use colored::{Colorize, ColoredString};
+use colored::{ColoredString, Colorize};
+use std::str::Lines;
 use term_size;
 
 pub enum State {
@@ -14,116 +14,83 @@ pub struct BeautifyLines {
     grep_word: Option<String>,
 }
 
-
 impl BeautifyLines {
-
     pub fn new(state: State, string: String) -> BeautifyLines {
         BeautifyLines {
-            state: state,
-            string: string,
+            state,
+            string,
             start_ind: 0,
             grep_word: None,
         }
-
     }
 
-    pub fn set_start_ind(&mut self, ind: usize) -> &Self{
+    pub fn set_start_ind(&mut self, ind: usize) -> &Self {
         self.start_ind = ind;
 
         self
     }
 
-    pub fn set_grep_blinker(&mut self, grep_word: String) -> &Self{
+    pub fn set_grep_blinker(&mut self, grep_word: String) -> &Self {
         self.grep_word = Some(grep_word);
 
         self
     }
 
-
-    pub fn print (&self) {
-
+    pub fn print(&self) {
         match self.state {
-            State::Header => self.print_header(),
             State::Body => self.print_body(),
+            State::Header => self.print_header(),
         }
-
     }
 
-
     fn print_header(&self) {
-
         let vec_str = Self::limit_lines(self.string.lines());
 
-        let header_str = vec_str.get(0).expect("We need at least one line of output to populate header");
+        let header_str = vec_str
+            .get(0)
+            .expect("We need at least one line of output to populate header");
 
         print!("{}\n", header_str.on_black().bright_white().bold());
-    
+
         for _ in 0..Self::get_max_len() {
             print!("=")
         }
 
         println!();
-
     }
 
-
     fn print_body(&self) {
-
         let vec_str = Self::limit_lines(self.string.lines());
 
         for i in self.start_ind..vec_str.len() {
-
             let line = vec_str.get(i).expect("");
 
             match &self.grep_word {
-                Some(grep_word) => {
-                    match line.find(grep_word) {
-                        Some(start_ind) => {
-                            let end_ind = start_ind + grep_word.len();
+                Some(grep_word) => match line.find(grep_word) {
+                    Some(start_ind) => {
+                        let end_ind = start_ind + grep_word.len();
 
-                            print! (
-                                "{}", 
-                                Self::custom_colorize(i, &line[0..start_ind])
-                            );
+                        print!("{}", Self::custom_colorize(i, &line[0..start_ind]));
 
-                            print! (
-                                "{}", 
-                                &line[start_ind..end_ind].blink()
-                            );
+                        print!("{}", &line[start_ind..end_ind].blink());
 
-                            print! (
-                                "{}", 
-                                Self::custom_colorize(i, &line[end_ind..])
-                            );
-                            
-                            println!();
+                        print!("{}", Self::custom_colorize(i, &line[end_ind..]));
 
-                        },
-                        None => {
-                            println! (
-                                "{}", 
-                                Self::custom_colorize(i, line)
-                            );
-                        },
+                        println!();
                     }
-
-
+                    None => {
+                        println!("{}", Self::custom_colorize(i, line));
+                    }
                 },
                 None => {
-                    println! (
-                        "{}", 
-                        Self::custom_colorize(i, line)
-                    );
-                },
+                    println!("{}", Self::custom_colorize(i, line));
+                }
             }
         }
-    
-        
     }
 
     fn custom_colorize(i: usize, line: &str) -> ColoredString {
-
-        match i  % 4 {
+        match i % 4 {
             0 => line.red(),
             1 => line.bright_green(),
             2 => line.yellow(),
@@ -131,27 +98,26 @@ impl BeautifyLines {
         }
     }
 
-
     fn limit_lines(lines: Lines) -> Vec<&str> {
-
         let max_len = Self::get_max_len();
 
-        let output_new = lines.map(|line| {
-            if line.len() < max_len {
-                return line
-            }
-    
-            &line[0..max_len]
-        }).collect::<Vec<&str>>();
+        let output_new = lines
+            .map(|line| {
+                if line.len() < max_len {
+                    return line;
+                }
+
+                &line[0..max_len]
+            })
+            .collect::<Vec<&str>>();
 
         output_new
-
     }
 
     fn get_max_len() -> usize {
-        let max_len = term_size::dimensions().expect("Couldn't get the terminal size").0;
+        let max_len = term_size::dimensions()
+            .expect("Couldn't get the terminal size")
+            .0;
         max_len
     }
-
-
 }
--- src/child.rs
+++ src/child.rs
@@ -1,4 +1,7 @@
-use std::{process::{Command, Stdio}, io::Write};
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
 
 pub struct ChildPipedProcess<'a> {
     name: &'a str,
@@ -7,20 +10,21 @@ pub struct ChildPipedProcess<'a> {
     output: Vec<u8>,
 }
 
-impl <'a> ChildPipedProcess <'a>{
-
-
-    pub fn new(name: &'a str, args: &'a [&'a str], piped_input: Option<&'a Vec<u8>>) -> ChildPipedProcess<'a> {
+impl<'a> ChildPipedProcess<'a> {
+    pub fn new(
+        name: &'a str,
+        args: &'a [&'a str],
+        piped_input: Option<&'a Vec<u8>>,
+    ) -> ChildPipedProcess<'a> {
         ChildPipedProcess {
-            name: name,
-            args: args,
-            piped_input: piped_input,
+            name,
+            args,
+            piped_input,
             output: vec![],
         }
     }
 
     pub fn process_output(&mut self) -> &Self {
-
         match self.piped_input {
             None => {
                 let child = Command::new(self.name)
@@ -30,53 +34,41 @@ impl <'a> ChildPipedProcess <'a>{
                     .spawn()
                     .expect("couldn't create the child");
 
-
                 drop(self.args);
 
-                let mut header_output = child
-                    .wait_with_output()
-                    .expect("child-out").stdout;
-        
-                self.output.append(&mut header_output);
-            },
-            Some(data) => {
-                
-                for b in data.chunks(32000) {
+                let mut header_output = child.wait_with_output().expect("child-out").stdout;
 
+                self.output.append(&mut header_output);
+            }
+            Some(data) => {
+                for b in data.chunks(32000) {
                     let mut process = Command::new(self.name)
-                    .args(self.args)
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("couldn't create the child");
+                        .args(self.args)
+                        .stdin(Stdio::piped())
+                        .stdout(Stdio::piped())
+                        .spawn()
+                        .expect("couldn't create the child");
 
                     let child_stdin = process.stdin.as_mut().unwrap();
 
+                    child_stdin
+                        .write(b)
+                        .expect("Something went wrong with writing chunks to the pipe");
 
-                    child_stdin.write(b).expect("Something went wrong with writing chunks to the pipe");
-
-
-                    let mut header_output = process
-                        .wait_with_output()
-                        .expect("child-out").stdout;
+                    let mut header_output = process.wait_with_output().expect("child-out").stdout;
 
                     self.output.append(&mut header_output);
-
-
-                    
                 }
-
-            },
+            }
         };
-
 
         self
     }
 
     pub fn output_to_str(&self) -> String {
-        let out = String::from_utf8(self.output.to_vec()) 
+        let out = String::from_utf8(self.output.to_vec())
             .expect("something went wrong with converting u8 vec to str");
         out
     }
-
 }
+
